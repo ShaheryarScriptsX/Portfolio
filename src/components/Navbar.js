9137f6c835de4dbf56a67bc2a74fcaacc2d09a64
shaheryarPL <syedshaheryar00008@gmail.com>
2025-02-26 01:26:10 +0500

    Updated the information in about section , footer and header.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,12 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
+          <span
+            style={{ fontWeight: "bold", color: "purple", fontSize: "33px" }}
+          >
+            Ss.
+          </span>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -83,34 +88,34 @@ function NavBar() {
             </Nav.Item>
 
             <Nav.Item>
-              <Nav.Link
+              {/* <Nav.Link
                 as={Link}
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
+              </Nav.Link> */}
             </Nav.Item>
 
             <Nav.Item>
-              <Nav.Link
+              {/* <Nav.Link
                 href="https://soumyajitblogs.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
+              </Nav.Link> */}
             </Nav.Item>
 
             <Nav.Item className="fork-btn">
-              <Button
+              {/* <Button
                 href="https://github.com/soumyajit4419/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
+              </Button> */}
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
